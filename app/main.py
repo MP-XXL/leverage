31fdd3d80de1e77937d1099f9ea607e63fe472f9
MP-XXL <royceapera@yahoo.com>
2025-12-10 06:52:37 -0600

    Feat: Adds alembic

--- app/main.py
+++ app/main.py
@@ -1,37 +1,16 @@
 from fastapi import FastAPI
-from .models.base import Base
-from .database.database_main import engine
+from models.base import Base
+from database.database_main import engine
 import time
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
-def db_and_table_init():
-    retries = 10
-    for i in range(retries):
-        try:
-            logger.info("STARTING APPLICATION!")
-            Base.metadata.create_all(bind=engine)
-            logger.info("DATABASE INITIALIZED SUCCESSFULLY!")
-            break
-        except OperationalError as e:
-            logger.warning(f"MySQL NOT READY, RETRYING ({i+1}/{retries}) {e}...")
-            time.sleep(3)
-        except Exception as e:
-            logger.info(f"DATABASE INITIALIZATION FAILED: {e}")
-
-
 app = FastAPI(
     title = "Leverage",
     version = "0.0.1 Beta"
 )
-
-@app.on_event("startup")
-def on_startup():
-    db_and_table_init()
-
 
 @app.get("/")
 def home():
---
+++ app/models/users_model.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, Enum, Integer, DateTime, ForeignKey, func, Date
+from .base import Base
+from enums import Verification
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    first_name = Column(String(64), max_length=64, nullable=False)
+    last_name = Column(String(64), max_length=64, nullable=False)
+    leverage_tag = Column(String(10), nullable=True)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    phone = Column(String(15), unique=True, nullable=False)
+    date_of_birth = Column(Date, nullable=True)
+    address = Column(String(256), nullable=True)
+    #nationality = Column(Enum(Nationality))
+    verification = Column(Enum(Verification), default=Verification.UNVERIFIED, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
